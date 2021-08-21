e1858426a2969f0dd3370eba65320684d99d4225
Ivan <miltonivan117@gmail.com>
2021-08-21 11:32:41 -0600

    form system to add more products

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { FC, useEffect, useState } from "react";
+import { FC, FormEvent, useEffect, useState } from "react";
 
 interface Info {
   id: number;
@@ -20,9 +20,14 @@ interface Data {
 }
 [];
 
+interface Img {
+  name:string
+}
+
 const App: FC = () => {
   const [data, setData] = useState<Data[]>([]);
   const [info, setInfo] = useState<Info>({} as Info);
+  const [imgPrev, setImgPrev] = useState(null);
 
   const url = "https://fakestoreapi.com/products";
 
@@ -31,22 +36,46 @@ const App: FC = () => {
     const data = await resp.json();
     setData(data);
   };
-  const handleChange = (event: any) => {
+  const handleChange = (event: {
+    target: { name: string; value: string | number };
+  }) => {
     setInfo({
       ...info,
       [event.target.name]: event.target.value,
     });
   };
-  const handleSubmit = (event: any) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     setData([
-      ...data,
       {
         ...info,
         id: data.length + 1,
       },
+      ...data,
     ]);
+    setInfo({
+      title: "",
+    } as Info);
+  };
+  const hanldeImg = (event: any) => {
+    const uploadImg = event.target.files[0]
+    const types = ["image/png","image/jpeg","image/jpg"]
+
+    if (uploadImg && types.includes(uploadImg.type)) {
+      let reader = new FileReader()
+      reader.onloadend= ()=>{
+        setImgPrev(reader.result)
+        console.log(reader)
+      }
+      reader.readAsDataURL(uploadImg)
+      console.log(reader)
+
+    }else{
+      console.log("no selected")
+    }
+
+    // console.log(img);
   };
 
   useEffect(() => {
@@ -54,11 +83,20 @@ const App: FC = () => {
     useFetch(url);
   }, []);
 
-  console.log(data);
+  console.log(imgPrev);
   return (
     <>
       <div>
-        <h1>FakeApi</h1>
+        <h1>FakeApi Store</h1>
+        <div style={
+          {
+            width: "100px",
+            height: "100px",
+            background: imgPrev ? `url("${imgPrev}") no-repeat center/cover` : "black"
+          }
+        }>
+
+        </div>
         <form onSubmit={handleSubmit}>
           <input
             onChange={handleChange}
@@ -67,7 +105,13 @@ const App: FC = () => {
             type="text"
             placeholder="Title"
           />
-          <input type="text" onChange={handleChange} name="price" value={info.price} placeholder="price"/>
+          <input
+            type="text"
+            onChange={handleChange}
+            name="price"
+            value={info.price}
+            placeholder="price"
+          />
           <input
             onChange={handleChange}
             name="description"
@@ -75,22 +119,34 @@ const App: FC = () => {
             type="text"
             placeholder="description"
           />
-          <input type="text" onChange={handleChange} name="category" value={info.category} placeholder="category" />
+          <input
+            type="text"
+            onChange={handleChange}
+            name="category"
+            value={info.category}
+            placeholder="category"
+          />
+          <input type="file" name="imagen" onChange={hanldeImg} />
           <button type="submit">Send</button>
         </form>
       </div>
-      <div style={{display:"flex", flexWrap:"wrap"}}>
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
         {data.map((product) => (
-          <div key={`key ${product.id}c4${product.title} `}  style={{margin:"40px"}}>
-            <h4 style={{width:"400px"}}>{product.title}</h4>
+          <div
+            key={`key${product.id}-${product.title} `}
+            style={{ margin: "40px" }}
+          >
+            <h4 style={{ width: "400px" }}>{product.title}</h4>
             <h6>{product.category}</h6>
             <img
               style={{ width: "200px", height: "200px" }}
               src={product.image}
               alt={product.title}
             />
-            <p><b>${product.price}</b></p>
-            <p style={{width:"200px"}}>{product.description}</p>
+            <p>
+              <b>${product.price}</b>
+            </p>
+            <p style={{ width: "200px" }}>{product.description}</p>
           </div>
         ))}
       </div>
